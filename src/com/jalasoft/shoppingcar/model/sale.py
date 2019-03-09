8e948abcfc4db4d31cde5b1d6927ae349aa058e7
Kely Flores <kely.flores.ticona@outlook.com>
2019-03-09 09:31:33 -0400

    initial model

---
+++ src/com/jalasoft/shoppingcar/model/sale.py
@@ -0,0 +1,10 @@
+
+class Sale:
+    def __init__(self):
+        self.__item_list = []
+
+    def add_item(self, item):
+        self.__item_list.append(item)
+
+    def remove_item(self, item):
+        self.remove_item(item)
---
+++ src/com/jalasoft/shoppingcar/model/sale_detail.py
@@ -0,0 +1,17 @@
+from src.com.jalasoft.shoppingcar.model.item import Item
+
+
+class SaleDetail:
+
+    def __init__(self, item_list: Item):
+        self.__item_list = item_list
+        self.__quantity = self.__item_list.count(self)
+
+    def calculate_price_total(self):
+        total = 0
+        for item in self.__item_list:
+            total += item.get_price()
+        return total
+
+    def print_detail(self):
+        pass
