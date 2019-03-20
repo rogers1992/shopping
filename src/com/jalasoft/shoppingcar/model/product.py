28cff1f8bd7c25cdc7a4bae00d4d910e56aa157b
Kely Flores <kely.flores.ticona@outlook.com>
2019-03-20 09:31:22 -0400

    fix observations

--- src/com/jalasoft/shoppingcar/model/product.py
+++ src/com/jalasoft/shoppingcar/model/product.py
@@ -1,11 +1,16 @@
 
-class Item(object):
-    def __init__(self, name,  price):
+class Product(object):
+    def __init__(self, id_product, name,  price):
+        self.__id = id_product
         self.__name = name
         self.__price = price
+
+    def get_id(self):
+        return self.__id
 
     def get_name(self):
         return self.__name
 
     def get_price(self):
         return self.__price
+
--- src/com/jalasoft/shoppingcar/model/sale.py
+++ src/com/jalasoft/shoppingcar/model/sale.py
@@ -1,13 +1,25 @@
+from src.com.jalasoft.shoppingcar.db.product_query import ProductQuery
 
 class Sale:
     def __init__(self):
-        self.__item_list = []
+        self.__product_list = {}
 
-    def add_item(self, item):
-        self.__item_list.append(item)
+    def add_product(self, product):
+        self.__product_list[product.get_id()] = product
 
-    def remove_item(self, item):
-        self.remove_item(item)
+    def save_product(self, product):
+        print(product)
+        query = ProductQuery()
+        query.insertProduct(product)
 
-    def get_item_all(self):
-        return self.__item_list
+    def remove_product(self, id_product):
+        for key, value in dict(self.__product_list).items():
+            if key == id_product:
+                del self.__product_list[id_product]
+
+    def get_product_all(self):
+        return self.__product_list
+
+    def get_all_product(self):
+        query = ProductQuery()
+        return query.loadAllProduct()
